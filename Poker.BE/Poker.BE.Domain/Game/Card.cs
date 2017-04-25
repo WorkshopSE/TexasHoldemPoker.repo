b5c27a06bb164ba2d789ac995566cd84ccd2ceac
gwainer <gwainer@live.com>
2017-04-25 20:59:37 +0300

    card & deck classes

--- Poker.BE/Poker.BE.Domain/Game/Card.cs
+++ Poker.BE/Poker.BE.Domain/Game/Card.cs
@@ -6,9 +6,43 @@ using System.Threading.Tasks;
 
 namespace Poker.BE.Domain.Game
 {
-    public class Card
+	public enum Suit
+	{
+		Hearts,
+		Clubs,
+		Spades,
+		Diamonds
+	}
+	public enum Value
+	{
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+		Ace,
+	}
+	public class Card
     {
-        // TODO: complete - set team member to do this
+		// TODO: complete - set team member to do this
+		#region Properties
+		protected Suit CardSuit;
+		protected Value CardNumber;
+		#endregion
 
-    }
+		#region Methods
+		public Card(Suit suit, Value num)
+		{
+			CardSuit = suit;
+			CardNumber = num;
+		}
+		#endregion
+	}
 }
--- Poker.BE/Poker.BE.Domain/Game/Deck.cs
+++ Poker.BE/Poker.BE.Domain/Game/Deck.cs
@@ -8,7 +8,22 @@ namespace Poker.BE.Domain.Game
 {
     public class Deck
     {
-        // TODO: complete - set team member to do this
+		#region Properties
+		Dictionary<int, Card> DeckOfCards;
+		#endregion
+		// TODO: complete - set team member to do this
+		#region Methods
+		public Deck()
+		{
+			foreach (Value value in Enum.GetValues(typeof(Value)))
+				foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+					DeckOfCards.Add(1, new Card(suit, value));
 
-    }
+		}
+		protected void ShuffleCards()
+		{
+
+		}
+		#endregion
+	}
 }
