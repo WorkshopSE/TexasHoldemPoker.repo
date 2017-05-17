b0a48d747a3d1556b62b6e5e32338f6fbf91929e
Asaf Ben Aharon <asafb26@gmail.com>
2017-05-17 23:43:20 +0300

    added progress bar and background music

---
+++ Poker.FE/Assets/MainMenu/Scripts/UIControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+    public GameObject loadingText;
+    public GameObject loadingImage;
+	public void ChangeScene(string sceneName)
+    {
+        loadingText.SetActive(true);
+        loadingImage.SetActive(true);
+        Application.LoadLevel(sceneName);
+    }
+}
