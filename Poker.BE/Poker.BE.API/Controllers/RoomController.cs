c6fd335f00fb44c01c9753dfb9687233149e8a22
idanizi <idanizi@gmail.com>
2017-05-19 02:00:05 +0300

    initial room controller, todo excemption catching

--- Poker.BE/Poker.BE.API/Controllers/RoomController.cs
+++ Poker.BE/Poker.BE.API/Controllers/RoomController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Poker.BE.Service.Modules.Requests;
+using Poker.BE.Service.Modules.Results;
+using Poker.BE.Service.IServices;
+using Poker.BE.Service.Services;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +14,35 @@ namespace Poker.BE.API.Controllers
 {
     public class RoomController : ApiController
     {
+        #region Fields
+        private IRoomsService service;
+        #endregion
+
+        #region Constructors
+        public RoomController()
+        {
+            service = new RoomsService();
+        }
+        #endregion
+
+        #region Methods
+        [HttpPost]
+        public HttpResponseMessage EnterRoom(EnterRoomRequest request)
+        {
+            var result = default(EnterRoomResult);
+
+            try
+            {
+                result = service.EnterRoom(request);
+            }
+            catch (Exception)
+            {
+                // TODO
+                throw new NotImplementedException();
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+        #endregion
     }
 }
