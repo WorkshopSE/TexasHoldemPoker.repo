23a5ace2de95790efb3b1293c1c332729f443b95
gwainer <gwainer@live.com>
2017-05-19 18:55:02 +0300

    Autentication service fix

--- Poker.BE/Poker.BE.API/Controllers/AuthenticationController.cs
+++ Poker.BE/Poker.BE.API/Controllers/AuthenticationController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Poker.BE.Service.IServices;
+using Poker.BE.Service.Modules.Requests;
+using Poker.BE.Service.Modules.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,64 @@ namespace Poker.BE.API.Controllers
 {
     public class AuthenticationController : ApiController
     {
-    }
+		#region Fields
+		private IAuthenticationService service;
+		#endregion
+
+		#region Constructors
+		public AuthenticationController()
+		{
+			service = new Service.Services.AuthenticationService();
+		}
+		#endregion
+
+		#region Methods
+		[HttpPost]
+		public HttpResponseMessage Login(LoginRequest request)
+		{
+			var result = default(LoginResult);
+
+			try
+			{
+				result = service.Login(request);
+			}
+			catch (Exception e)
+			{
+				result.ErrorMessage = e.Message;
+			}
+
+			return Request.CreateResponse(HttpStatusCode.OK, result);
+		}
+		public HttpResponseMessage Logout(LogoutRequest request)
+		{
+			var result = default(LogoutResult);
+
+			try
+			{
+				result = service.Logout(request);
+			}
+			catch (Exception e)
+			{
+				result.ErrorMessage = e.Message;
+			}
+
+			return Request.CreateResponse(HttpStatusCode.OK, result);
+		}
+		public HttpResponseMessage SignUp(SignUpRequest request)
+		{
+			var result = default(SignUpResult);
+
+			try
+			{
+				result = service.SignUp(request);
+			}
+			catch (Exception e)
+			{
+				result.ErrorMessage = e.Message;
+			}
+
+			return Request.CreateResponse(HttpStatusCode.OK, result);
+		}
+		#endregion
+	}
 }
