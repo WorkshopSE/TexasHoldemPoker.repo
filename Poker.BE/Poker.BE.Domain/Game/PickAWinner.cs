f7b8f11e85ee57f007f34a0a3e45298e91f6d3b2
Ariel Tobiana <tobiana@post.bgu.ac.il>
2017-05-20 04:23:26 +0300

    getWinner is written and must be tested

--- Poker.BE/Poker.BE.Domain/Game/PickAWinner.cs
+++ Poker.BE/Poker.BE.Domain/Game/PickAWinner.cs
@@ -9,6 +9,9 @@ namespace Poker.BE.Domain.Game
 		#region Constants
 		public const int POWER = 14;
         public const int FIVEBESTCARDS = 5;
+        public const int NUMBERORDER = 10;
+		public const int FALSERESULT = -1;
+
 		#endregion
 
 		#region Fields
@@ -33,7 +36,72 @@ namespace Poker.BE.Domain.Game
 
 		#region Methods
         private Player GetWinner(){
-            return null;
+            bool winnerFounded = false, severalWinners = false;
+            int numberOfPlayers = PlayerSevenCards.Count, k=0, ans=0, winnerIndex=0;
+            int[] arrayWinners = new int[numberOfPlayers];
+            Player winner = null;
+            for (int i = NUMBERORDER; i > 0 && !winnerFounded; i--){
+                foreach ( Player player in Players){
+                    ans = CheckPlayerByOrder(player, i);
+                    arrayWinners[k] = ans;
+                    k++;
+                    if (ans != FALSERESULT) winnerFounded = true;
+                }
+                if(winnerFounded){
+                    for (int j = 0; j < arrayWinners.Length - 1; j++){
+                        int maxResult = arrayWinners[i];
+                        if (arrayWinners[i+1] > maxResult){
+                            maxResult = arrayWinners[i];
+                            winnerIndex = i;
+                            severalWinners = false;
+                        }
+                        else{
+                            severalWinners = true;
+                        }
+                    }
+                    if (!severalWinners){
+                        winner = getPlayerByDictionnaryAndIndex(winnerIndex);
+                    }
+                }
+                else{
+                    k = 0;
+                }
+            }
+            return winner;
+        }
+
+        private int CheckPlayerByOrder(Player player, int numberRound){
+            Card[] playerCardArray = null;
+            bool isThereACorrectCardArray = PlayerSevenCards.TryGetValue(player, out playerCardArray);
+
+            if (isThereACorrectCardArray){
+				switch (numberRound){
+                    case 1: return HighFiveCardValue(playerCardArray);
+                    case 2: return IsPair(playerCardArray);
+					case 3: return IsTwoPair(playerCardArray);
+                    case 4: return IsThreeOfAKind(playerCardArray);
+                    case 5: return IsStraight(playerCardArray);
+                    case 6: return IsFlush (playerCardArray);
+                    case 7: return IsFullHouse (playerCardArray);
+                    case 8: return IsFourOfAKind(playerCardArray);
+                    case 9: return IsStraightFlush(playerCardArray);
+                    case 10: return IsRoyalFlush(playerCardArray);
+					default:  return 0;
+			    }
+			}
+            return FALSERESULT;
+        }
+
+        private Player getPlayerByDictionnaryAndIndex (int playerIndex){
+            Player winner = null;
+            int currentIndex = 0;
+            foreach (Player player in Players){
+                if (currentIndex == playerIndex){
+                    winner = player;
+                }
+                currentIndex = currentIndex + 1;
+            }
+            return winner;
         }
 
         private void InitializeDictionnary(){
@@ -71,7 +139,8 @@ namespace Poker.BE.Domain.Game
 
 
         //We calculate the sum in this way - BestCard * POWER^ 4 + SecondBestCard * POWER^3..
-		private double HighFiveCardValue (Card[] PlayerAndTableCards) {
+        // NumberOrderIndex = 1
+		private int HighFiveCardValue (Card[] PlayerAndTableCards) {
             int i=0;
             int exponant = 4;
             double sum = 0;
@@ -86,12 +155,13 @@ namespace Poker.BE.Domain.Game
 				exponant = exponant - 1;
                 j = j - 1;
 			}
-            return sum;
+            return (int)sum;
 		}
 
+		// NumberOrderIndex = 2
 		private int IsPair(Card[] PlayerAndTableCards) {
-            int PairValue = -1;
-            for (int i = 0; i < PlayerAndTableCards.Length - 1 && PairValue== -1 ; i++) {
+            int PairValue = FALSERESULT;
+            for (int i = 0; i < PlayerAndTableCards.Length - 1 && PairValue== FALSERESULT ; i++) {
                 if (PlayerAndTableCards[i].CompareTo(PlayerAndTableCards[i+1]) == 0){
                     PairValue = PlayerAndTableCards[i].GetValueNumberWIthAs();
                 }
@@ -99,13 +169,14 @@ namespace Poker.BE.Domain.Game
             return PairValue;
 		}
 
+		// NumberOrderIndex = 3
 		private int IsTwoPair(Card[] PlayerAndTableCards) {
-			int TwoPairValue = -1;
+			int TwoPairValue = FALSERESULT;
             for (int i = 0; i < PlayerAndTableCards.Length - 1 && TwoPairValue < (POWER + 1); i++)
 			{
 				if (PlayerAndTableCards[i].CompareTo(PlayerAndTableCards[i + 1]) == 0)
 				{
-                    if (TwoPairValue == -1 ){ // the value of the first pair x * POWER^0
+                    if (TwoPairValue == FALSERESULT ){ // the value of the first pair x * POWER^0
 						TwoPairValue = PlayerAndTableCards[i].GetValueNumberWIthAs();
 					}
                     else{ // the value of the second pair - TwoPairValue + (x * POWER^1)
@@ -113,14 +184,15 @@ namespace Poker.BE.Domain.Game
                     }
 				}
 			}
-            if (TwoPairValue < (POWER + 1)) TwoPairValue = -1;
+            if (TwoPairValue < (POWER + 1)) TwoPairValue = FALSERESULT;
 			return TwoPairValue;
 
 		}
 
+		// NumberOrderIndex = 4
 		private int IsThreeOfAKind(Card[] PlayerAndTableCards) {
-			int ThreeOfAKindValue = -1;
-			for (int i = 0; i < PlayerAndTableCards.Length - 2 && ThreeOfAKindValue == -1; i++)
+			int ThreeOfAKindValue = FALSERESULT;
+			for (int i = 0; i < PlayerAndTableCards.Length - 2 && ThreeOfAKindValue == FALSERESULT; i++)
 			{
 				if (PlayerAndTableCards[i].CompareTo(PlayerAndTableCards[i + 1]) == 0) // pair
 				{
@@ -131,8 +203,8 @@ namespace Poker.BE.Domain.Game
 			return ThreeOfAKindValue;
 		}
 
-		
-        private int IsStraight (Card[] PlayerAndTableCards){
+		// NumberOrderIndex = 5
+		private int IsStraight (Card[] PlayerAndTableCards){
             if (IsAsStraight(PlayerAndTableCards) > 0) return IsAsStraight(PlayerAndTableCards);
             else return IsNormalStraight(PlayerAndTableCards);
         }
@@ -141,14 +213,14 @@ namespace Poker.BE.Domain.Game
             int i, MinimumStraightValueIndex = 0;
             int Straight = 1;
             for (i = 0; i < PlayerAndTableCards.Length - 1 && Straight < 5; i++){
-                if (PlayerAndTableCards[i].CompareTo(PlayerAndTableCards[i + 1]) == -1){
+                if (PlayerAndTableCards[i].CompareTo(PlayerAndTableCards[i + 1]) == FALSERESULT){
                     Straight = Straight + 1;
                 }
                 else{
                     MinimumStraightValueIndex = i;
                 }
             }
-            if (Straight < 5) return -1;
+            if (Straight < 5) return FALSERESULT;
             else return MinimumStraightValueIndex;
         }
 
@@ -168,9 +240,10 @@ namespace Poker.BE.Domain.Game
                 if (asStraightIndex[k] == 0) ans = false;
             }
             if (ans) return 10;
-            else return -1;
+            else return FALSERESULT;
         }
 
+		// NumberOrderIndex = 6
 		private int IsFlush (Card[] PlayerAndTableCards) {
             int i;
 			int[] FlushColor = new int[4];
@@ -183,11 +256,12 @@ namespace Poker.BE.Domain.Game
                     return 1;
                 }
             }
-            return -1;
+            return FALSERESULT;
 		}
 
+		// NumberOrderIndex = 7
 		private int IsFullHouse (Card[] PlayerAndTableCards) {
-            int sum = -1;
+            int sum = FALSERESULT;
             if (IsTwoPair(PlayerAndTableCards) > 0 && IsThreeOfAKind(PlayerAndTableCards) > 0) {
                 int PairValue = IsTwoPair(PlayerAndTableCards);
                 int ThreeOfAKindValue = IsThreeOfAKind(PlayerAndTableCards);
@@ -196,6 +270,7 @@ namespace Poker.BE.Domain.Game
             return sum;
 		}
 
+		// NumberOrderIndex = 8
         private int IsFourOfAKind(Card[] PlayerAndTableCards) {
             int i;
 			int Count = 1;
@@ -210,17 +285,18 @@ namespace Poker.BE.Domain.Game
                 }
 	
 			}
-            if (Count < 4) return -1;
+            if (Count < 4) return FALSERESULT;
             else return PlayerAndTableCards[i-1].GetValueNumberWIthAs();
 		}
 
+		// NumberOrderIndex = 9
 		private int IsStraightFlush(Card[] PlayerAndTableCards) {
             if (IsStraight(PlayerAndTableCards) > 0){
                 int smallStraightCardValue = IsStraight(PlayerAndTableCards);
-                int startStraightIndex = -1;
+                int startStraightIndex = FALSERESULT;
 
                 // we are looking for the last minimum straight value occurence in our array
-                for (int i = 2; i > 0 && startStraightIndex == -1; i--){
+                for (int i = 2; i > 0 && startStraightIndex == FALSERESULT; i--){
                     if (PlayerAndTableCards[i].GetValueNumber() == smallStraightCardValue){
                         startStraightIndex = i;
                     }
@@ -234,16 +310,17 @@ namespace Poker.BE.Domain.Game
                     }
                 }
                 if (ans) return smallStraightCardValue;
-                else return -1;
+                else return FALSERESULT;
             }
-			return -1;
+			return FALSERESULT;
 		}
 
+		// NumberOrderIndex = 10
         private int IsRoyalFlush (Card[] PlayerAndTableCards) {
             if (IsStraightFlush(PlayerAndTableCards) > 0 && IsAsStraight(PlayerAndTableCards) > 0){
                 return 1;
             }
-            return -1;
+            return FALSERESULT;
         }
 		#endregion
 
