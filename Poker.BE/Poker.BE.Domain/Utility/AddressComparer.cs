eff02ed1f4cac1a62a3690faf4cebc878590a36c
idanizi <idanizi@gmail.com>
2017-05-21 15:51:04 +0300

    working on find rooms

---
+++ Poker.BE/Poker.BE.Domain/Utility/AddressComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.BE.Domain.Utility
+{
+    public class AddressComparer<T> : IEqualityComparer<T>
+    {
+        public bool Equals(T x, T y)
+        {
+            return x.GetHashCode() == y.GetHashCode();
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
