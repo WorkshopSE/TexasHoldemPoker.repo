749aebe2da5d0201ad5e06240c9683d5e24f1aed
gwainer <gwainer@live.com>
2017-05-24 16:05:49 +0300

    pr fix

--- Poker.BE/Poker.BE.Service/Modules/Requests/LogoutRequest.cs
+++ Poker.BE/Poker.BE.Service/Modules/Requests/LogoutRequest.cs
@@ -8,6 +8,9 @@ namespace Poker.BE.Service.Modules.Requests
 {
 	public class LogoutRequest : IRequest
 	{
+		/// <summary>
+		/// requested user hash code
+		/// </summary>
 		public int User { get; set; }
 	}
 }
