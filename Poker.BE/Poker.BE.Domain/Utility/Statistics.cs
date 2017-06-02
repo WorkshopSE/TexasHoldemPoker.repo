acaf6e28c1a7342464c65f70418088d465fdbb03
tomer314 <tomeramdur@gmail.com>
2017-06-02 18:59:56 +0300

    build Statistics and StatisticsManager classes

--- Poker.BE/Poker.BE.Domain/Utility/Statistics.cs
+++ Poker.BE/Poker.BE.Domain/Utility/Statistics.cs
@@ -8,7 +8,45 @@ namespace Poker.BE.Domain.Utility
 {
     public class Statistics
     {
-        // TODO: complete - set team member to do this
+        #region Fields
+        private int gamesPlayed;
+        private double grossProfits;
+        private double grossLosses;
+        #endregion
 
+        #region Constructors
+        public Statistics()
+        {
+            gamesPlayed = 0;
+            grossProfits = 0;
+            grossLosses = 0;
+        }
+        #endregion
+
+        #region Methods
+        public void AddHandStatistic(int amountOfMoney)
+        {
+            if (amountOfMoney > 0)
+            {
+                grossProfits += amountOfMoney;
+            }
+            else
+            {
+                grossLosses -= amountOfMoney;
+            }
+
+            gamesPlayed++;
+        }
+
+        public double GetWinRate()
+        {
+            return (grossProfits - grossLosses) / gamesPlayed;
+        }
+
+        public double GetGrossProfitWinRate()
+        {
+            return grossProfits / gamesPlayed;
+        }
+        #endregion
     }
 }
