0fd16884c0982273cef82929fc8aa1fe7e60077d
idanizi <idanizi@gmail.com>
2017-06-10 04:30:50 +0300

    creating data layer, repository design pattern

---
+++ Poker.BE/Poker.BE.Data/Access.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.BE.Data
+{
+    /// <summary>
+    /// Unit Of Work for Repository pattern, disposable.
+    /// </summary>
+    class Access : IDisposable
+    {
+        private MainContext _context;
+
+        public Access(MainContext context)
+        {
+            _context = context;
+            // TODO: initiate repositories (= new...)
+        }
+
+        #region Repositories Exposure
+        // TODO public repositoryclass name {get; private set;}
+        #endregion
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Poker.BE/Poker.BE.Data/MainContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.BE.Data
+{
+    /// <summary>
+    /// DBContext for data entities for entity framework
+    /// </summary>
+    public class MainContext : DbContext
+    {
+        // TODO
+        /*
+         * DbSet<..> for all entities
+         * */
+    }
+}
