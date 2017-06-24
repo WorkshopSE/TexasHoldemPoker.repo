fc6813ded0f61e8e142810cb7d4620f742c545e8
tomer314 <tomeramdur@gmail.com>
2017-06-24 16:22:45 +0300

    added fields to createNewRoom result

--- Poker.BE/Poker.BE.Service/Modules/Results/CreateNewRoomResult.cs
+++ Poker.BE/Poker.BE.Service/Modules/Results/CreateNewRoomResult.cs
@@ -8,16 +8,26 @@ namespace Poker.BE.Service.Modules.Results
 {
 	public class CreateNewRoomResult : IResult
 	{
-		/// <summary>
-		/// room hash code
-		/// </summary>
-		public int Room { get; set; }
-
+		public int Room { get; set; }   //room hash code
         public string RoomName { get; set; }
+		public int Player { get; set; } //player hash code 
 
-		/// <summary>
-		/// player hash code 
-		/// </summary>
-		public int Player { get; set; }
-	}
+
+        /// <summary>
+        /// Request's info
+        /// </summary>
+        public int Level { get; set; }
+        public string User { get; set; }
+
+        //Game Preferences data
+        public string Name { get; set; }
+        public double BuyInCost { get; set; }
+        public double MinimumBet { get; set; }
+        public double Antes { get; set; }
+        public int MinNumberOfPlayers { get; set; }
+        public int MaxNumberOfPlayers { get; set; }
+        public bool IsSpactatorsAllowed { get; set; }
+
+        public double Limit { get; set; }       //this is for decorator (PotLimit/Limit game mode)
+    }
 }
